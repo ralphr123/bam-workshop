af5a2bce799e78263a5b0a9345260e7357d59024
Ralph Rouhana <ralph.rouhana@gmail.com>
2023-07-31 23:37:01 -0400

    Move variable set up in ex1 to work area

--- src/_EDIT_THESE_FILES/exercise1.py
+++ src/_EDIT_THESE_FILES/exercise1.py
@@ -7,16 +7,11 @@ import os
 # OpenAI Python SDK to allow us to make requests to GPT
 import openai
 
-# Store OpenAI environment variables
-openai.api_type = 'azure'
-openai.api_version = '2023-05-15'
-openai.api_base = os.getenv('OPENAI_API_BASE')
-openai.api_key = os.getenv('OPENAI_API_KEY')
+
 
 # Use this function to print to the console for debugging
 def printToConsole(*args, sep=' ', end='\n'):
     print(*args, sep=sep, end=end, flush=True)
-
 
 # ===========================
 # Welcome! This is the part of the file where you'll be writing your code.
@@ -28,6 +23,12 @@ def printToConsole(*args, sep=' ', end='\n'):
 # ===========================
 # === MODIFY THIS SECTION ===
 # ===========================
+
+# Set OpenAI environment variables as shown in documenation
+openai.api_type = 'azure'
+openai.api_version = '2023-05-15'
+openai.api_base = os.getenv('OPENAI_API_BASE')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Call Azure to send the message to GPT and return the response string
 def sendMessage(message: str) -> str:
